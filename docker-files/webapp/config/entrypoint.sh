a576e80ba91e01997c05545e2418f8d332fe8af2
bishwashere <b.bishwas@gmail.com>
2019-06-04 12:48:26 +0530

    changes

--- docker-files/webapp/config/entrypoint.sh
+++ docker-files/webapp/config/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-PATH=/usr/local/bin:$PATH
-python /home/app_user/pg_manager/src/webapp/manage.py collectstatic --noinput
-python /home/codaxtr_user/codaxtr-extractor/src/webapp/manage.py syncdb --noinput --database al_auth
-# Create superuser if initial setup
-python home/app_user/pg_manager/src/webapp/manage.py shell <<END
-END
-exec $@
+# #!/bin/sh
+# PATH=/usr/local/bin:$PATH
+# python /home/app_user/pg_manager/src/webapp/manage.py collectstatic --noinput
+# python /home/codaxtr_user/codaxtr-extractor/src/webapp/manage.py syncdb --noinput --database al_auth
+# # Create superuser if initial setup
+# python home/app_user/pg_manager/src/webapp/manage.py shell <<END
+# END
+# exec $@
