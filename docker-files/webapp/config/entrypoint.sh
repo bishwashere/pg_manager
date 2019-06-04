af16782729d1956a6b1b9c7d1423828e8c2d70f8
bishwashere <b.bishwas@gmail.com>
2019-06-04 12:47:05 +0530

    changes

--- docker-files/webapp/config/entrypoint.sh
+++ docker-files/webapp/config/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 PATH=/usr/local/bin:$PATH
-python /home/app_user/pg_manager/src/manage.py collectstatic --noinput
-python /home/codaxtr_user/codaxtr-extractor/src/codaxtr_site/manage.py syncdb --noinput --database al_auth
+python /home/app_user/pg_manager/src/webapp/manage.py collectstatic --noinput
+python /home/codaxtr_user/codaxtr-extractor/src/webapp/manage.py syncdb --noinput --database al_auth
 # Create superuser if initial setup
-python /home/codaxtr_user/codaxtr-extractor/src/codaxtr_site/manage.py shell <<END
-from django.contrib.auth.models import User
-User.objects.filter(username="admin").exists() or User.objects.create_superuser("admin", "admin@example.com", "admin")
+python home/app_user/pg_manager/src/webapp/manage.py shell <<END
 END
-
 exec $@
